bd035e44d0e5cddb2a2f465136fb35d120f63dea
Ezo-mas <122978172+Ezo-mas@users.noreply.github.com>
2025-03-07 15:15:10 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,6 +28,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
