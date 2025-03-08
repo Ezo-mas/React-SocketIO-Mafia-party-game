df31f326def24afbce9ba94f62a015af889bb59c
Ezo-mas <122978172+Ezo-mas@users.noreply.github.com>
2025-03-08 12:41:20 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,24 +2,30 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 const { Server } = require('socket.io');
 
 const app = express();
 app.use(cors()); // Allow cross-origin requests
+
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: '*' }  // Allow connections from any origin
 });
 
+
 io.on('connection', (socket) => {
   console.log('a user connected:', socket.id);
   
-  // Example: listen for a custom event
-  socket.on('example_event', (data) => {
-    console.log('Received:', data);
-    // Optionally broadcast or emit a response
-    io.emit('response_event', { msg: 'Hello from server!' });
+  socket.on('join_room', (roomId, username) => {
+    socket.join(roomId);
+    io.to(roomId).emit('player_joined', username);
+  });
+
+  socket.on('start_game', (roomId) => {
+    io.to(roomId).emit('game_started');
   });
 
   socket.on('disconnect', () => {
@@ -27,12 +33,9 @@ io.on('connection', (socket) => {
   });
 });
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, '../client/build')));
-
 // For any other route, serve the index.html file from the build
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
