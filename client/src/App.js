5b08a3ef581868f2be3668fc11108ccdf82d6bbc
Ezo-mas <aluskarstas@gmail.com>
2025-03-27 15:11:10 +0200

    - fixed css serving to the client and removed redundant functions

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import TitlePage from './components/TitlePage';
 import LobbyPage from './components/LobbyPage';
 import LandingPage from './components/LandingPage.jsx';
 import AboutPage from './components/AboutPage';
-import GamePage from './components/GamePage'; // Import GamePage
+import GamePage from './components/GamePage';
 
 function App() {
   return (
@@ -21,14 +21,7 @@ function App() {
                 <div className="link"><Link to="/" id="home-link">Home</Link></div>
                 <div className="link"><Link to="/about" id="about-link">About Us</Link></div>
               </div>
-              <i className="fa-solid fa-bars hamburg" onClick={() => window.hamburg()}></i>
-            </div>
-            <div className="dropdown">
-              <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About Us</Link>
-                <i className="fa-solid fa-xmark cancel" onClick={() => window.cancel()}></i>
-              </div>
+              
             </div>
           </nav>
           <Routes>
